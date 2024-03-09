0132b2497f5d719139da9d2295fb980223e4fc43
Biplove Gautam <biplovegautam123@gmail.com>
2024-03-09 08:23:24 +0545

    Update Disc.tsx

--- src/components/HeadingAndDisc/Disc.tsx
+++ src/components/HeadingAndDisc/Disc.tsx
@@ -1,13 +1,13 @@
 
-export const MakeHeading = (p) => {
-  return (
-    <h1>{p.text}</h1>
-  )
-}
+// export const MakeHeading = (p) => {
+//   return (
+//     <h1>{p.text}</h1>
+//   )
+// }
 
 
-export const MakeDiscription = (p) => {
-  return (
-    <p>{p.text}</p>
-  )
-}
+// export const MakeDiscription = (p) => {
+//   return (
+//     <p>{p.text}</p>
+//   )
+// }
