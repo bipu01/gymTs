6dda033dde965a6203d449d4ec9f1086ca433ff2
Biplove Gautam <biplovegautam123@gmail.com>
2024-03-14 19:10:03 +0545

    menu toggle ui finished

---
+++ src/components/ProgramMenu/ProgramMenu.tsx
@@ -0,0 +1,25 @@
+
+const ProgramMenu = () => {
+  return (
+    <>
+      <div className="">
+        <p className="opacity-50 text-xs xmd:text-sm mb-2 ">*Your data and images are not distributed and are private</p>
+        <input type="text" placeholder="Full name" className="w-95% xmd:w-35% bg-transparent border-2 rounded-sm p-1"/>
+      </div>
+
+      <input type="text" placeholder="Ph num" className="w-95% xmd:w-35% bg-transparent border-2 rounded-sm p-1"/>
+
+      <div className="flex items-center">
+        <p>Add your Photo:</p>
+        <p className="opacity-50 text-xs xmd:text-sm">(*for id card)</p>
+      </div>
+      <div>
+      <label htmlFor="select">
+        <input type="file" accept="image/*" id="select"/>
+      </label>
+      </div>
+    </>
+  )
+}
+
+export default ProgramMenu
